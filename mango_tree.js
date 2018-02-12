6cc42a3bcba4d90fb26adacfd3f3107a12eadb20
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-12 15:57:22 +0700

    Release 0

--- mango_tree.js
+++ mango_tree.js
@@ -6,23 +6,32 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
+    this._age = 0
+    this._height = 0
+    this._fruits = 0
+    this._maxfruit = 10
+    this._harvested = 0
+    this._healtyStatus = null
   }
 
   get age() {
+    return this._age
   }
 
   get height() {
-
+    return this._height
   }
 
   get fruits() {
+    return this._fruits
   }
 
   get healtyStatus() {
+    return this._healtyStatus
   }
 
   get harvested() {
-
+    return this._harvested
   }
 
 
@@ -45,6 +54,7 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor() {
+    this._quality = null
   }
 }
 
@@ -60,9 +70,12 @@ class Mango {
   */
 
 // Release 1
-class AppleTree {}
-class Apple {}
+class AppleTree { }
+class Apple { }
 
 // Release 2
-class FruitTree {}
-class Fruit {}
+class FruitTree { }
+class Fruit { }
+
+let mangotree = new MangoTree()
+console.log(mangotree)
