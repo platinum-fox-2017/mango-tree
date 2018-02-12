31dd4eac2481766a5e0f0507cc0743f238e3dfa5
agnynureza <rezaagny@gmail.com>
2018-02-12 19:35:40 +0700

    done

---
+++ fruit_tree.js
@@ -0,0 +1,34 @@
+class FruitTree{
+  constructor(age,height,fruits,harvest,healtyStatus) {
+    this._height = 0
+    this._age = 0
+    this._fruits = []
+    this._healthStatus = true
+    this._harvest = 0
+  }
+
+  get age() {
+    return this._age
+  }
+
+  get height() {
+    return this._height
+  }
+
+  get fruits() {
+    return this._fruits
+
+  }
+
+  get healthyStatus() {
+    return this._healthStatus
+  }
+
+
+  get harvested() {
+    return this._harvest
+  }
+
+}
+
+module.exports = FruitTree
