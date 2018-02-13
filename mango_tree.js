1cee85d4fdf4a7fd30f425519016f7dd60a21a72
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-13 09:00:47 +0700

    tambah keterangan

--- mango_tree.js
+++ mango_tree.js
@@ -5,7 +5,7 @@ class MangoTree {
     this._age = 0;
     this._height = 0;
     this._fruits = 0;
-    this._harvested = 0; //total yang dipetik
+    this._harvested = 0;
     this._harvest = [];
     this._status = true;
     this.maxAge = 19;
@@ -28,6 +28,7 @@ class MangoTree {
   }
 
   get harvested() {
+    //pengelompokan hasil panen
     let good = 0;
     let bad = 0;
     for(let i=0; i<this._harvest.length; i++){
@@ -41,13 +42,16 @@ class MangoTree {
   }
 
   grow() {
+    //kalo udah sampe umur maksimal, berhenti looping
     if(this._age===this.maxAge){
       this._status = false;
     }
     this._age++;
+    //masih tumbuh kalo umurnya dibawah 15 tahun
     if(this._age<15) {
       this._height += Math.round((Math.random()*15)/10);
     }
+    // berbuah setelah 3 tahun
     if(this._age>3){
       console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} m | Fruits harvested = ${mangoTree.harvested}`);
     } else {
@@ -56,10 +60,12 @@ class MangoTree {
   }
 
   produceMangoes() {
+    //hasil panen total
     this._fruits = Math.floor(Math.random()*10)+1;
   }
 
   harvest() {
+    //menilai kualitas hasil panen
     let array = [];
     for(let i=0; i<this._fruits; i++) {
       let quality = Math.floor(Math.random()*2)
